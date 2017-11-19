9cf04b2111a7a9e3fe3cd8ec6173eb62438853c6
ASUS\User <aishabibi.adambek@nu.edu.kz>
2017-11-19 18:20:34 +0600

    Commit 19/11

--- app.py
+++ app.py
@@ -1,3 +1,6 @@
+import os
+import uuid
+
 from flask import Flask, render_template, request, json, session
 from werkzeug.security import generate_password_hash, check_password_hash
 from flaskext.mysql import MySQL
@@ -11,12 +14,48 @@ app.config['MYSQL_DATABASE_USER'] = 'root'
 app.config['MYSQL_DATABASE_PASSWORD'] = 'ran21Dom03!'
 app.config['MYSQL_DATABASE_DB'] = 'media_sharing_platform'
 app.config['MYSQL_DATABASE_HOST'] = 'localhost'
+app.config['UPLOAD_FOLDER'] = 'static/Uploads'
 mysql.init_app(app)
+pageLimit = 2
+
+
+"""class StreamConsumingMiddleware(object):
+
+    def __init__(self, app):
+        self.app = app
+
+    def __call__(self, environ, start_response):
+        stream = LimitedStream(environ['wsgi.input'],
+                               int(environ['CONTENT_LENGTH'] or 0))
+        environ['wsgi.input'] = stream
+        app_iter = self.app(environ, start_response)
+        try:
+            stream.exhaust()
+            for event in app_iter:
+                yield event
+        finally:
+            if hasattr(app_iter, 'close'):
+                app_iter.close()
+
+app.config['UPLOAD_FOLDER'] = 'static/Uploads'
+app.wsgi_app = StreamConsumingMiddleware(app.wsgi_app)"""
 
 
 @app.route("/")
 def main():
     return render_template('index.html')
+
+
+@app.route('/upload', methods=['GET', 'POST'])
+def upload():
+    if request.method == 'POST':
+        file = request.files['file']
+        extension = os.path.splitext(file.filename)[1]
+        print(extension)
+        f_name = str(uuid.uuid4()) + extension
+        print(f_name)
+        file.save(os.path.join(app.config['UPLOAD_FOLDER'], f_name))
+        return json.dumps({'filename': f_name})
 
 
 @app.route("/showSignUp")
@@ -129,27 +168,40 @@ def addWish():
         conn.close()
 
 
-@app.route('/getItem')
+@app.route('/getItem', methods=['POST'])
 def getItem():
     try:
         print("here")
         if session.get('user'):
 
             _user_id = session.get('user')
-
-            print("HERE")
+            _limit = pageLimit
+            _offset = request.form['offset']
+            _total = 0
 
             con = mysql.connect()
             cursor = con.cursor()
-            cursor.callproc('sp_GetItemByUser', (_user_id,))
+            cursor.callproc('sp_GetItemByUser', (_user_id, _limit, _offset, _total))
             items = cursor.fetchall()
 
+            cursor.close()
+
+            cursor = con.cursor()
+            cursor.execute('select @_sp_GetItemByUser_3')
+
+            outParam = cursor.fetchall()
+            print(outParam)
+            response = []
             items_dict = []
             for item in items:
                 item_dict = {'Id': item[0], 'Title': item[1], 'Description': item[2], 'Date': item[4]}
                 items_dict.append(item_dict)
 
-            return json.dumps(items_dict)
+            response.append(items_dict)
+            response.append({'total': outParam[0][0]})
+            print(response)
+
+            return json.dumps(response)
         else:
             return render_template('error.html', error='Unauthorized Access')
     except Exception as e:
@@ -202,6 +254,33 @@ def updateItem():
     finally:
         cursor.close()
         conn.close()
+
+
+@app.route('/deleteItem', methods=['POST'])
+def deleteItem():
+    try:
+        if session.get('user'):
+            _id = request.form['id']
+            _user = session.get('user')
+
+            conn = mysql.connect()
+            cursor = conn.cursor()
+            cursor.callproc('sp_deleteItem', (_id, _user))
+            result = cursor.fetchall()
+
+            if len(result) is 0:
+                conn.commit()
+                return json.dumps({'status': 'OK'})
+            else:
+                return json.dumps({'status': 'An Error occured'})
+        else:
+            return render_template('error.html', error='Unauthorized Access')
+    except Exception as e:
+        return json.dumps({'status': str(e)})
+    finally:
+        cursor.close()
+        conn.close()
+
 
 if __name__ == "__main__":
     app.run(port=5002)
